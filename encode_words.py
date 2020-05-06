27ca58b23d2abd53ca9bb32c032c6d3802c070ad
SimicSvetislav <simicsvetislav@gmail.com>
2020-05-06 10:29:14 +0200

    Pretvaranje prepoznatih ključnih reči u vektor obeležja

--- encode_words.py
+++ encode_words.py
@@ -23,13 +23,59 @@ def encode(words_list):
             encodings[i] = encodings[i] | encoding_dictionary[word]
     
     
-    return encodings
+    return encodings, len(encoding_dictionary)
+
+
+def disassemble_binary(bin_number, no_encodings):
+    
+    # print(no_encodings)
+    
+    bin_list = [0] * no_encodings
+    mask = 0b1
+    
+    bin_length = bin_number.bit_length()
+    
+    # print("Length :", bin_length)
+    
+    for i in range(bin_length):
+    
+        bit = bin_number & mask
+        
+        bin_list[i] = bit
+        
+        bin_number = bin_number>>1 
+    
+    # print(bin_list)
+        
+    if len(bin_list) != no_encodings:
+        raise
+    
+    bin_list.reverse()
+    
+    return bin_list
+
+
+def process_encoding(words_list):
+    
+    encodings, no_encodings = encode(words_list)
+    
+    for encoding in encodings:
+        print(format(encoding, '#06b'))
+        
+    encodings_list = []    
+    
+    for i, encoding in enumerate(encodings):
+        encodings_list.append(disassemble_binary(encodings[i], no_encodings))
+    
+    return encodings_list
 
 if __name__=="__main__":
     s1 = [['cat', 'dog'], ['cat', 'bird'], ['monkey']]
 
-    encodings = encode(s1)
+    encodings_list = process_encoding(s1)
     
-    for encoding in encodings:
-        print(format(encoding, '#06b'))
+    print(encodings_list)
+    
+    for encoding in encodings_list:
+        print(encoding)
     
